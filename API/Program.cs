aca9d06402d3f8cfa0ad23b52a6a9126623aa1c6
Joe Cui <xiaozhou.cui@gmail.com>
2022-04-10 12:37:21 +1000

    Add migration service to Main method

--- API/Program.cs
+++ API/Program.cs
@@ -1,9 +1,7 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using API.Data;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 
@@ -14,7 +12,27 @@ namespace API
         public static void Main(string[] args)
         {
             // Create Kestrel server
-            CreateHostBuilder(args).Build().Run();
+            var host = CreateHostBuilder(args).Build();
+            // Create scope to add service, keyword "using" will auto disopse the scope when it's no longer used
+            using var scope = host.Services.CreateScope();
+            // Add services into scope
+            var context = scope.ServiceProvider.GetRequiredService<StoreContext>();
+            var logger = scope.ServiceProvider.GetRequiredService<ILogger<Program>>();
+            // run scoped services in a try-catch block
+            try
+            {
+                // programatically run migration, instead of using CLI command `dotnet ef database update`
+                context.Database.Migrate();
+                // pupulate database if no data found
+                DbInitializer.Initialize(context);
+            }
+            catch (System.Exception ex)
+            {
+                logger.LogError(ex, "Problem migrating data");
+            }
+
+            // run the server
+            host.Run();
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>

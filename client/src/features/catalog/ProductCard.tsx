36e1d86a9fa54277e7461920da853f20f56a9ac4
Joe Cui <xiaozhou.cui@gmail.com>
2022-04-17 22:23:34 +1000

    Apply MUI grid layout

--- client/src/features/catalog/ProductCard.tsx
+++ client/src/features/catalog/ProductCard.tsx
@@ -1,4 +1,11 @@
-import { ListItem, ListItemAvatar, Avatar, ListItemText } from '@mui/material';
+import {
+  Button,
+  Card,
+  CardActions,
+  CardContent,
+  CardMedia,
+  Typography,
+} from '@mui/material';
 import { Product } from '../../app/models/product';
 
 interface Props {
@@ -7,13 +14,26 @@ interface Props {
 
 export default function ProductCard({ product }: Props) {
   return (
-    <ListItem>
-      <ListItemAvatar>
-        <Avatar src={product.pictureUrl}></Avatar>
-      </ListItemAvatar>
-      <ListItemText>
-        {product.name} - {product.price}
-      </ListItemText>
-    </ListItem>
+    <Card>
+      <CardMedia
+        component="img"
+        height="140"
+        image="http://picsum.photos/200"
+        alt="green iguana"
+      />
+      <CardContent>
+        <Typography gutterBottom variant="h5" component="div">
+          Lizard
+        </Typography>
+        <Typography variant="body2" color="text.secondary">
+          Lizards are a widespread group of squamate reptiles, with over 6,000
+          species, ranging across all continents except Antarctica
+        </Typography>
+      </CardContent>
+      <CardActions>
+        <Button size="small">Share</Button>
+        <Button size="small">Learn More</Button>
+      </CardActions>
+    </Card>
   );
 }

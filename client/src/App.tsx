6e3578afddf65590f10e18fa2ae01e10ab09686b
Joe Cui <xiaozhou.cui@gmail.com>
2022-04-17 13:39:58 +1000

    Replace template in app.ts

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,24 +1,38 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from 'react';
 
 function App() {
+  const [products, setProducts] = useState([
+    { name: 'product1', price: 100.0 },
+    { name: 'product2', price: 200.0 },
+  ]);
+
+  useEffect(() => {
+    fetch('http://localhost:5000/api/products')
+      .then((response) => response.json())
+      .then((data) => setProducts(data));
+  }, []);
+
+  function addProduct() {
+    setProducts((prevState) => [
+      ...prevState,
+      {
+        name: 'product' + (prevState.length + 1),
+        price: prevState.length * 100 + 100,
+      },
+    ]);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Re-Store</h1>
+      <ul>
+        {products.map((item, index) => (
+          <li key={index}>
+            {item.name} - {item.price}
+          </li>
+        ))}
+      </ul>
+      <button onClick={addProduct}>Add Product</button>
     </div>
   );
 }

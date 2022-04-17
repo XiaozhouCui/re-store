268862e2781114fddf7d9052b48e72afa3a22f79
Joe Cui <xiaozhou.cui@gmail.com>
2022-04-17 22:09:20 +1000

    Add AppBar as header

--- client/src/app/layout/App.tsx
+++ client/src/app/layout/App.tsx
@@ -1,7 +1,8 @@
-import { Typography } from '@mui/material';
+import { Container, CssBaseline } from '@mui/material';
 import { useEffect, useState } from 'react';
 import Catalog from '../../features/catalog/Catalog';
 import { Product } from '../models/product';
+import Header from './Header';
 
 function App() {
   const [products, setProducts] = useState<Product[]>([]);
@@ -28,8 +29,11 @@ function App() {
 
   return (
     <>
-      <Typography variant="h1">Re-Store</Typography>
-      <Catalog products={products} addProduct={addProduct} />
+      <CssBaseline />
+      <Header />
+      <Container>
+        <Catalog products={products} addProduct={addProduct} />
+      </Container>
     </>
   );
 }

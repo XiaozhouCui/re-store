430411c1484448551e3328aa3d2bea21d1ebfdc7
Joe Cui <xiaozhou.cui@gmail.com>
2022-04-18 20:14:38 +1000

    Add MUI switch for dark mode

--- client/src/app/layout/App.tsx
+++ client/src/app/layout/App.tsx
@@ -15,13 +15,20 @@ function App() {
   const theme = createTheme({
     palette: {
       mode: paletteType,
+      background: {
+        default: paletteType === 'light' ? '#eaeaea' : '#121212',
+      },
     },
   });
+
+  const handleThemeChange = () => {
+    setDarkMode(!darkMode);
+  };
 
   return (
     <ThemeProvider theme={theme}>
       <CssBaseline />
-      <Header />
+      <Header darkMode={darkMode} handleThemeChange={handleThemeChange} />
       <Container>
         <Catalog />
       </Container>

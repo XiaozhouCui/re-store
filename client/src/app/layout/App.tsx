dea01976341e0211936cb6688562fed38b441d2c
Joe Cui <xiaozhou.cui@gmail.com>
2022-04-22 22:35:12 +1000

    Add toastify

--- client/src/app/layout/App.tsx
+++ client/src/app/layout/App.tsx
@@ -3,25 +3,27 @@ import {
   createTheme,
   CssBaseline,
   ThemeProvider,
-} from '@mui/material';
-import { useState } from 'react';
-import { Route } from 'react-router-dom';
-import AboutPage from '../../features/about/AboutPage';
-import Catalog from '../../features/catalog/Catalog';
-import ProductDetails from '../../features/catalog/ProductDetails';
-import ContactPage from '../../features/contact/ContactPage';
-import HomePage from '../../features/home/HomePage';
-import Header from './Header';
+} from "@mui/material";
+import { useState } from "react";
+import { Route } from "react-router-dom";
+import { ToastContainer } from "react-toastify";
+import AboutPage from "../../features/about/AboutPage";
+import Catalog from "../../features/catalog/Catalog";
+import ProductDetails from "../../features/catalog/ProductDetails";
+import ContactPage from "../../features/contact/ContactPage";
+import HomePage from "../../features/home/HomePage";
+import Header from "./Header";
+import "react-toastify/dist/ReactToastify.css";
 
 function App() {
   const [darkMode, setDarkMode] = useState(false);
-  const paletteType = darkMode ? 'dark' : 'light';
+  const paletteType = darkMode ? "dark" : "light";
   // set Dark Mode using MUI theme
   const theme = createTheme({
     palette: {
       mode: paletteType,
       background: {
-        default: paletteType === 'light' ? '#eaeaea' : '#121212',
+        default: paletteType === "light" ? "#eaeaea" : "#121212",
       },
     },
   });
@@ -32,6 +34,7 @@ function App() {
 
   return (
     <ThemeProvider theme={theme}>
+      <ToastContainer position="bottom-right" theme="colored" hideProgressBar />
       <CssBaseline />
       <Header darkMode={darkMode} handleThemeChange={handleThemeChange} />
       <Container>

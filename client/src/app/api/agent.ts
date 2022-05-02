95e2288e15395b7319810d62e0e1836b86854a19
Joe Cui <xiaozhou.cui@gmail.com>
2022-05-02 11:50:12 +1000

    Adding the axios methods for the basket

--- client/src/app/api/agent.ts
+++ client/src/app/api/agent.ts
@@ -6,6 +6,8 @@ const sleep = () => new Promise((resolve) => setTimeout(resolve, 500));
 
 // all requests url will be pre-fixed with this base url
 axios.defaults.baseURL = "http://localhost:5000/api/";
+// allow receive/set cookies in browser
+axios.defaults.withCredentials = true;
 
 const responseBody = (response: AxiosResponse) => response.data;
 
@@ -72,9 +74,17 @@ const TestErrors = {
   getValidationError: () => requests.get("buggy/validation-error"),
 };
 
+const Basket = {
+  // cookies "buyerId" included in req
+  get: () => requests.get("basket"),
+  addItem: (productId: number, quantity = 1) => requests.post(`basket?productId=${productId}&quantity=${quantity}`, {}),
+  removeItem: (productId: number, quantity = 1) => requests.delete(`basket?productId=${productId}&quantity=${quantity}`),
+}
+
 const agent = {
   Catalog,
   TestErrors,
+  Basket,
 };
 
 export default agent;
--- client/src/features/catalog/ProductCard.tsx
+++ client/src/features/catalog/ProductCard.tsx
@@ -1,3 +1,4 @@
+import { LoadingButton } from '@mui/lab';
 import {
   Avatar,
   Button,
@@ -8,7 +9,9 @@ import {
   CardMedia,
   Typography,
 } from '@mui/material';
+import { useState } from 'react';
 import { Link } from 'react-router-dom';
+import agent from '../../app/api/agent';
 import { Product } from '../../app/models/product';
 
 interface Props {
@@ -16,6 +19,14 @@ interface Props {
 }
 
 export default function ProductCard({ product }: Props) {
+  const [loading, setLoading] = useState(false);
+  const handleAddItem = (productId: number) => {
+    setLoading(true);
+    agent.Basket.addItem(productId)
+      .catch((error) => console.log(error))
+      .finally(() => setLoading(false));
+  };
+
   return (
     <Card>
       <CardHeader
@@ -47,7 +58,13 @@ export default function ProductCard({ product }: Props) {
         </Typography>
       </CardContent>
       <CardActions>
-        <Button size="small">Add to cart</Button>
+        <LoadingButton
+          loading={loading}
+          onClick={() => handleAddItem(product.id)}
+          size="small"
+        >
+          Add to cart
+        </LoadingButton>
         <Button component={Link} to={`/catalog/${product.id}`} size="small">
           View
         </Button>

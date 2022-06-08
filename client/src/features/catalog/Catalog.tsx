810819ab4a4da7fe1f169edb9a8f6bd6ff9760b1
Joe Cui <xiaozhou.cui@gmail.com>
2022-06-08 14:30:42 +1000

    Catalog page design

--- client/src/features/catalog/Catalog.tsx
+++ client/src/features/catalog/Catalog.tsx
@@ -1,3 +1,17 @@
+import {
+  Box,
+  Checkbox,
+  FormControl,
+  FormControlLabel,
+  FormGroup,
+  Grid,
+  Pagination,
+  Paper,
+  Radio,
+  RadioGroup,
+  TextField,
+  Typography,
+} from '@mui/material';
 import { useEffect } from 'react';
 import LoadingComponent from '../../app/layout/LoadingComponent';
 import { useAppDispatch, useAppSelector } from '../../app/store/configureStore';
@@ -8,12 +22,17 @@ import {
 } from './catalogSlice';
 import ProductList from './ProductList';
 
+const sortOptions = [
+  { value: 'name', label: 'Alphabetical' },
+  { value: 'priceDesc', label: 'Price - High to low' },
+  { value: 'price', label: 'Price - Low to high' },
+];
+
 export default function Catalog() {
   // get NORMALIZED products by using createEntityAdapter from redux-toolkit
   const products = useAppSelector(productSelectors.selectAll); // productSelectors has all the useful methods
-  const { productsLoaded, filtersLoaded, status } = useAppSelector(
-    (state) => state.catalog
-  );
+  const { productsLoaded, filtersLoaded, status, brands, types } =
+    useAppSelector((state) => state.catalog);
   const dispatch = useAppDispatch();
   useEffect(() => {
     // no need to re-fetch products everytime
@@ -28,8 +47,58 @@ export default function Catalog() {
     return <LoadingComponent message="Loading products..." />;
 
   return (
-    <>
-      <ProductList products={products} />
-    </>
+    <Grid container spacing={4}>
+      <Grid item xs={3}>
+        <Paper sx={{ mb: 2 }}>
+          <TextField label="Search Products" variant="outlined" fullWidth />
+        </Paper>
+        <Paper sx={{ mb: 2, p: 2 }}>
+          <FormControl>
+            <RadioGroup>
+              {sortOptions.map(({ value, label }) => (
+                <FormControlLabel
+                  key={value}
+                  value={value}
+                  control={<Radio />}
+                  label={label}
+                />
+              ))}
+            </RadioGroup>
+          </FormControl>
+        </Paper>
+        <Paper sx={{ mb: 2, p: 2 }}>
+          <FormGroup>
+            {brands.map((brand) => (
+              <FormControlLabel
+                key={brand}
+                control={<Checkbox />}
+                label={brand}
+              />
+            ))}
+          </FormGroup>
+        </Paper>
+        <Paper sx={{ mb: 2, p: 2 }}>
+          <FormGroup>
+            {types.map((type) => (
+              <FormControlLabel
+                key={type}
+                control={<Checkbox />}
+                label={type}
+              />
+            ))}
+          </FormGroup>
+        </Paper>
+      </Grid>
+      <Grid item xs={9}>
+        <ProductList products={products} />
+      </Grid>
+      <Grid item xs={3} />
+      <Grid item xs={9}>
+        <Box display="flex" justifyContent="space-between" alignItems="center">
+          <Typography>Displaying 1-6 of 20 items</Typography>
+          <Pagination color="secondary" size="large" count={10} page={2} />
+        </Box>
+      </Grid>
+    </Grid>
   );
 }

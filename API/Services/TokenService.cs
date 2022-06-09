08ec723fc348a0e4dfcefaefc96c41c309b0583b
Joe Cui <xiaozhou.cui@gmail.com>
2022-06-09 15:45:43 +1000

    Creating a token service to be injected

---
+++ API/Services/TokenService.cs
@@ -0,0 +1,58 @@
+using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+using API.Entities;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Localization;
+using Microsoft.IdentityModel.Tokens;
+
+// Token Service will be injected
+namespace API.Services
+{
+    public class TokenService
+    {
+        private readonly UserManager<User> _userManager;
+        private readonly IConfiguration _config;
+        public TokenService(UserManager<User> userManager, IConfiguration config)
+        {
+            _config = config;
+            _userManager = userManager;
+        }
+
+        public async Task<string> GenerateToken(User user)
+        {
+            // add username and email to the claims
+            var claims = new List<Claim>
+            {
+                new Claim(ClaimTypes.Email, user.Email),
+                new Claim(ClaimTypes.Name, user.UserName),
+            };
+            // add role to the claims
+            var roles = await _userManager.GetRolesAsync(user);
+            // a user can have many roles (e.g. admin & member)
+            foreach (var role in roles)
+            {
+                claims.Add(new Claim(ClaimTypes.Role, role));
+            }
+            // create the key to sign and decrypt JWT
+            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_config["JWTSettings:TokenKey"]));
+            // create signing credential with the key
+            var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha512);
+            // add JWT options
+            var tokenOptions = new JwtSecurityToken(
+                issuer: null,
+                audience: null,
+                claims: claims,
+                expires: DateTime.Now.AddDays(7),
+                signingCredentials: creds
+            );
+            // return JWT
+            return new JwtSecurityTokenHandler().WriteToken(tokenOptions);
+        }
+    }
+}

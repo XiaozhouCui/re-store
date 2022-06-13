b9407c52a7d8e1a670fc4c843f2d56649d3c50af
Joe Cui <xiaozhou.cui@gmail.com>
2022-06-13 20:35:26 +1000

    Creating the Orders page

--- client/src/app/layout/App.tsx
+++ client/src/app/layout/App.tsx
@@ -25,6 +25,7 @@ import Login from '../../features/account/Login';
 import Register from '../../features/account/Register';
 import { fetchCurrentUser } from '../../features/account/accountSlice';
 import PrivateRoute from './PrivateRoute';
+import Orders from '../../features/orders/Orders';
 // import { useStoreContext } from '../context/StoreContex';
 
 function App() {
@@ -79,6 +80,7 @@ function App() {
           <Route path="/contact" component={ContactPage} />
           <Route path="/basket" component={BasketPage} />
           <PrivateRoute path="/checkout" component={CheckoutPage} />
+          <PrivateRoute path="/orders" component={Orders} />
           <Route path="/login" component={Login} />
           <Route path="/register" component={Register} />
           <Route path="/server-error" component={ServerError} />

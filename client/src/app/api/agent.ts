00f5edd9680b207314294a56939752ed300c3bd3
Joe Cui <xiaozhou.cui@gmail.com>
2022-06-15 09:20:51 +1000

    Call payment API from CheckoutWrapper

--- client/src/app/api/agent.ts
+++ client/src/app/api/agent.ts
@@ -122,12 +122,17 @@ const Orders = {
   create: (values: any) => requests.post('orders', values),
 };
 
+const Payments = {
+  createPaymentIntent: () => requests.post('payment', {}),
+};
+
 const agent = {
   Catalog,
   TestErrors,
   Basket,
   Account,
   Orders,
+  Payments,
 };
 
 export default agent;
--- client/src/features/checkout/CheckoutWrapper.tsx
+++ client/src/features/checkout/CheckoutWrapper.tsx
@@ -1,5 +1,10 @@
 import { Elements } from '@stripe/react-stripe-js';
 import { loadStripe } from '@stripe/stripe-js';
+import { useEffect, useState } from 'react';
+import agent from '../../app/api/agent';
+import LoadingComponent from '../../app/layout/LoadingComponent';
+import { useAppDispatch } from '../../app/store/configureStore';
+import { setBasket } from '../basket/basketSlice';
 import CheckoutPage from './CheckoutPage';
 
 // pass in the publishable key from Stripe
@@ -10,6 +15,19 @@ const stripePromise = loadStripe(
 // wrap the Stripe provider around the CheckoutPage
 // https://stripe.com/docs/stripe-js/react#elements-provider
 const CheckoutWrapper = () => {
+  const dispatch = useAppDispatch();
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    // response will include the basket with paymentIntent and clientSecret
+    agent.Payments.createPaymentIntent()
+      .then((basket) => dispatch(setBasket(basket)))
+      .catch((error: any) => console.log(error))
+      .finally(() => setLoading(false));
+  }, [dispatch]);
+
+  if (loading) return <LoadingComponent message="Loading checkout..." />;
+
   return (
     <Elements stripe={stripePromise}>
       <CheckoutPage />

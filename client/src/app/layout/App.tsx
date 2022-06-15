ee5ba7efdb794b318e0a3a7b1288bbcfe321784a
Joe Cui <xiaozhou.cui@gmail.com>
2022-06-15 15:01:56 +1000

    Add carousel in HomePage

--- client/src/app/layout/App.tsx
+++ client/src/app/layout/App.tsx
@@ -71,22 +71,27 @@ function App() {
       <ToastContainer position="bottom-right" theme="colored" hideProgressBar />
       <CssBaseline />
       <Header darkMode={darkMode} handleThemeChange={handleThemeChange} />
-      <Container>
-        <Switch>
-          <Route exact path="/" component={HomePage} />
-          <Route exact path="/catalog" component={Catalog} />
-          <Route path="/catalog/:id" component={ProductDetails} />
-          <Route path="/about" component={AboutPage} />
-          <Route path="/contact" component={ContactPage} />
-          <Route path="/basket" component={BasketPage} />
-          <PrivateRoute path="/checkout" component={CheckoutWrapper} />
-          <PrivateRoute path="/orders" component={Orders} />
-          <Route path="/login" component={Login} />
-          <Route path="/register" component={Register} />
-          <Route path="/server-error" component={ServerError} />
-          <Route component={NotFound} />
-        </Switch>
-      </Container>
+      <Route exact path="/" component={HomePage} />
+      <Route
+        path={'/(.+)'} // regex for '/any-path'
+        render={() => (
+          <Container  sx={{ mt: 4 }}>
+            <Switch>
+              <Route exact path="/catalog" component={Catalog} />
+              <Route path="/catalog/:id" component={ProductDetails} />
+              <Route path="/about" component={AboutPage} />
+              <Route path="/contact" component={ContactPage} />
+              <Route path="/basket" component={BasketPage} />
+              <PrivateRoute path="/checkout" component={CheckoutWrapper} />
+              <PrivateRoute path="/orders" component={Orders} />
+              <Route path="/login" component={Login} />
+              <Route path="/register" component={Register} />
+              <Route path="/server-error" component={ServerError} />
+              <Route component={NotFound} />
+            </Switch>
+          </Container>
+        )}
+      />
     </ThemeProvider>
   );
 }

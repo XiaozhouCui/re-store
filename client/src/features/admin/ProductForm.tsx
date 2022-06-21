32a6dd673afd72a74f385c5dbd7cd2b15cb323ee
Joe Cui <xiaozhou.cui@gmail.com>
2022-06-21 15:07:55 +1000

    Add react-dropzone to upload image

--- client/src/features/admin/ProductForm.tsx
+++ client/src/features/admin/ProductForm.tsx
@@ -1,6 +1,7 @@
 import { Typography, Grid, Paper, Box, Button } from '@mui/material';
 import { useEffect } from 'react';
-import { useForm } from 'react-hook-form';
+import { FieldValues, useForm } from 'react-hook-form';
+import AppDropzone from '../../app/components/AppDropzone';
 import AppSelectList from '../../app/components/AppSelectList';
 import AppTextInput from '../../app/components/AppTextInput';
 import useProducts from '../../app/hooks/useProducts';
@@ -12,75 +13,102 @@ interface Props {
 }
 
 const ProductForm = ({ product, cancelEdit }: Props) => {
-  const { control, reset } = useForm();
+  const { control, reset, handleSubmit, watch } = useForm();
   const { brands, types } = useProducts();
+  // watchFile is to preview the uploaded image, "file" is the name of dropzone
+  const watchFile = watch('file', null);
 
   useEffect(() => {
     if (product) reset(product);
   }, [product, reset]);
+
+  const handleSubmitData = (data: FieldValues) => {
+    console.log(data);
+  };
 
   return (
     <Box component={Paper} sx={{ p: 4 }}>
       <Typography variant="h4" gutterBottom sx={{ mb: 4 }}>
         Product Details
       </Typography>
-      <Grid container spacing={3}>
-        <Grid item xs={12} sm={12}>
-          <AppTextInput control={control} name="name" label="Product name" />
+      <form onSubmit={handleSubmit(handleSubmitData)}>
+        <Grid container spacing={3}>
+          <Grid item xs={12} sm={12}>
+            <AppTextInput control={control} name="name" label="Product name" />
+          </Grid>
+          <Grid item xs={12} sm={6}>
+            <AppSelectList
+              control={control}
+              items={brands}
+              name="brand"
+              label="Brand"
+            />
+          </Grid>
+          <Grid item xs={12} sm={6}>
+            <AppSelectList
+              control={control}
+              items={types}
+              name="type"
+              label="Type"
+            />
+          </Grid>
+          <Grid item xs={12} sm={6}>
+            <AppTextInput
+              control={control}
+              type="number"
+              name="price"
+              label="Price"
+            />
+          </Grid>
+          <Grid item xs={12} sm={6}>
+            <AppTextInput
+              control={control}
+              type="number"
+              name="quantityInStock"
+              label="Quantity in Stock"
+            />
+          </Grid>
+          <Grid item xs={12}>
+            <AppTextInput
+              control={control}
+              multiline={true}
+              rows={4}
+              name="description"
+              label="Description"
+            />
+          </Grid>
+          <Grid item xs={12}>
+            <Box
+              display="flex"
+              justifyContent="space-between"
+              alignItems="center"
+            >
+              <AppDropzone control={control} name="file" />
+              {watchFile ? (
+                <img
+                  src={watchFile.preview}
+                  alt="preview"
+                  style={{ maxHeight: 200 }}
+                />
+              ) : (
+                <img
+                  src={product?.pictureUrl}
+                  alt={product?.name}
+                  style={{ maxHeight: 200 }}
+                />
+              )}
+            </Box>
+          </Grid>
         </Grid>
-        <Grid item xs={12} sm={6}>
-          <AppSelectList
-            control={control}
-            items={brands}
-            name="brand"
-            label="Brand"
-          />
-        </Grid>
-        <Grid item xs={12} sm={6}>
-          <AppSelectList
-            control={control}
-            items={types}
-            name="type"
-            label="Type"
-          />
-        </Grid>
-        <Grid item xs={12} sm={6}>
-          <AppTextInput
-            control={control}
-            type="number"
-            name="price"
-            label="Price"
-          />
-        </Grid>
-        <Grid item xs={12} sm={6}>
-          <AppTextInput
-            control={control}
-            type="number"
-            name="quantityInStock"
-            label="Quantity in Stock"
-          />
-        </Grid>
-        <Grid item xs={12}>
-          <AppTextInput
-            control={control}
-            multiline={true}
-            rows={4}
-            name="description"
-            label="Description"
-          />
-        </Grid>
-        <Grid item xs={12}>
-          <AppTextInput control={control} name="pictureUrl" label="Image" />
-        </Grid>
-      </Grid>
-      <Box display="flex" justifyContent="space-between" sx={{ mt: 3 }}>
-        <Button onClick={cancelEdit} variant="contained" color="inherit">
-          Cancel
-        </Button>
-        <Button variant="contained" color="success">
-          Submit
-        </Button>
-      </Box>
+        <Box display="flex" justifyContent="space-between" sx={{ mt: 3 }}>
+          <Button onClick={cancelEdit} variant="contained" color="inherit">
+            Cancel
+          </Button>
+          <Button type="submit" variant="contained" color="success">
+            Submit
+          </Button>
+        </Box>
+      </form>
     </Box>
   );
 };
